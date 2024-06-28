ed8892ea1a139756184f38d8d7451ccf7b505aea
Artem Borodovoy <a.borodovoi@gmail.com>
2024-06-28 13:39:40 +0300

    added icons swipe card

--- client/utils/config.js
+++ client/utils/config.js
@@ -1,5 +1,5 @@
 // utils config.js
 // export const BASE_URL = 'http://192.168.1.90:5000';
-export const BASE_URL = 'http://172.20.10.4:5000';
+export const BASE_URL = 'http://172.20.10.2:5000';
 export const TMDB_URL = 'https://api.themoviedb.org/3';
 export const TMDB_API_KEY = 'fb16b356ee60f42317bc4549500d69c4';
